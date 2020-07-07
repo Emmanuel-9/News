f8e61918add9aeb829e4eff383301c319cb4d4de
Emmanuel-9 <oregadaniel181@gmail.com>
2020-07-07 15:22:22 +0300

    Add the setUp method and test instance test case method.

--- tests/test_news.py
+++ tests/test_news.py
@@ -1,1 +1,11 @@
 import unittest
+from app.models import News
+
+class NewsTest(unittest.TestCase):
+
+  def setUp(self):
+    self.new_news = News('Rains','Michelle Tush','Expected rains in the month of April','https://images.unsplash.com/photo-1555089673-0b4b69c076d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80','2019/03/12T06:40:00Z','Farmers get ready for the season')
+
+  def test_instance(self):
+    self.assertTrue(isinstance(self.new_news,News))
+
