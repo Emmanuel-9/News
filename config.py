520749264ec489efb798bfcfd1bc9e746b55fc9d
Emmanuel-9 <oregadaniel181@gmail.com>
2020-07-10 18:36:47 +0300

    Add NEWS to the api key variable.

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 class Config:
   NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
   SOURCE_API_BASE_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-  NEWS_API_KEY = os.environ.get('API_KEY')
+  NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 
 class ProdConfig(Config):
