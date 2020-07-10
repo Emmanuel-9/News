bcc04d896722f51620515edac5a97d487b84202e
Emmanuel-9 <oregadaniel181@gmail.com>
2020-07-10 22:18:44 +0300

    Import NEWS API KEY from config.py in the instance folder.

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
+from instance.config import NEWS_API_KEY
+
 class Config:
   NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}'
   SOURCE_API_BASE_URL ='https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
-  NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+  NEWS_API_KEY = NEWS_API_KEY
 
 
 class ProdConfig(Config):
